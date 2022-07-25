9b2ce62ca03277bff71df97179a607f194c418f0
Ru1ka <wotb5ks@gmail.com>
2022-07-25 23:33:01 +0300

    inital coommit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    pass
+
+
+
+# Inst
+# Vk
+# Discord
+# Telegram 
+# GitHub
+# Email
+# Steam
