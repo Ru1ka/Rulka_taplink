4b5b4b527b6f4464be96d20b1c9505b97cdf1b84
Ru1ka <wotb5ks@gmail.com>
2022-08-03 14:39:55 +0300

    вырезал лишнее

--- static/main.js
+++ static/main.js
@@ -2,13 +2,6 @@ function discord_copy() {
     var copyText = document.getElementById("hide_txt");
     copyText.select();
     document.execCommand("copy");
-}
-
-
-function discord_copy() {
-    var copyText = document.getElementById("hide_txt");
-    copyText.select();
-    document.execCommand("copy");
     
     var tooltip = document.getElementById("tooltip");
     tooltip.innerHTML = "Copied: " + copyText.value;
